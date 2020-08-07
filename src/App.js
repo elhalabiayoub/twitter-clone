da887a74a493277217a1e79040a2c33b4303fcac
unknown <elhalabiayoub6@gmail.com>
2020-08-07 19:22:39 +0100

    the first commit of twitter-clone

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+	return (
+		//BEM
+		<div className="app">
+			<p>hello world , here it will be a twitter clone application</p>
+
+			{/*Sidebar*/}
+
+			{/*Feed*/}
+
+			{/*Widgets*/}
+		</div>
+	);
+}
+
+export default App;
